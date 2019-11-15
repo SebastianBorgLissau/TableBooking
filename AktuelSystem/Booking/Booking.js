ee76aa31f99b39db29880634c735ba3558855ffc
SebastianBorgLissau <seli19ab@student.cbs.dk>
2019-11-15 17:07:14 +0100

    demo of askebranch. AdminPage workings

---
+++ AktuelSystem/Booking/Booking.js
@@ -0,0 +1,169 @@
+/*this is our booking booking class*/
+
+class Booking {
+
+    constructor(bookingNumber, seatsChosen, date, time, user) {
+
+        this.bookingNumber = bookingNumber;
+        this.seatsChosen = seatsChosen;
+        this.date = date;
+        this.time = time;
+        this.user = user;
+    }
+
+    bookingAlert() {
+        alert("you have made a new booking");
+
+    }
+
+    bookingAlertDelete () {
+        alert("Booking " + this.bookingNumber + " succesfully deleted");
+    }
+
+    bookingAlertDeleteEmpty () {
+        alert("type the number of the booking you want to delete")
+    }
+}
+
+
+
+function setBooking() {
+
+    if (confirm('Are you sure, you want to make a booking?')) {
+        // Save it!
+
+        let bookingArray = JSON.parse(localStorage.getItem("bookings"));
+
+        var bookingNumber, seats2, e, date, f, time, userId, bookings, values;
+
+        if (bookingArray == null) {
+            bookingNumber = 1;
+        } else {
+            bookingNumber = bookingArray.length + 1;
+        }
+
+        seats2 = document.getElementById("tableSeats").value;
+        e = document.getElementById("date");
+        date = e.options[e.selectedIndex].value;
+
+        f = document.getElementById("time");
+        time = f.options[f.selectedIndex].value;
+
+        userId = localStorage.getItem("current_user");
+
+
+        bookings = new Booking(bookingNumber, seats2, date, time, userId);
+
+        values = JSON.parse(localStorage.getItem("bookings"));
+        if (values === null) {
+            values = [];
+        }
+        values.push(bookings);
+
+        localStorage.setItem("bookings", JSON.stringify(values));
+        new Booking().bookingAlert();
+        //  alert("new booking made");
+    }
+}
+
+function deleteBooking() {
+
+    let bookings = JSON.parse(localStorage.getItem("bookings"));
+    // let inputName = prompt("Type in the number of the booking you want to delete");
+    let iName = document.getElementById("deleteB").value;
+    var ib = bookings.findIndex(x => x.bookingNumber == iName);
+
+    if(iName == "") {
+        new Booking().bookingAlertDeleteEmpty();
+        //alert("type the number of the booking you want to delete");
+        return false;
+    }
+
+    if(ib !== -1) {
+
+        console.log(ib);
+        bookings.splice(ib, 1);
+        localStorage.setItem('bookings', JSON.stringify(bookings));
+        alert("booking successfully deleted");
+        location.reload();
+    } else {
+        new Booking().bookingAlertDelete();
+        return false;
+    }
+}
+
+function myBookings() {
+    let i,
+        bookings = JSON.parse(localStorage.getItem("bookings")), //skiftes ud med booking
+        inputName =localStorage.getItem("current_user"), //skiftes ud med identifier
+        bookingArray = [];
+
+    for(i = 0; i < bookings.length; i++) {
+        console.log(bookings[i]);
+
+        if (bookings[i].user == inputName) {
+            console.log("found");
+            bookingArray.push(bookings[i]);
+
+        } else {
+            console.log("not found")
+        }
+    }
+
+    let row, cell, text, r, c,
+        prop = ['bookingNumber','seatsChosen', 'date', 'time'], //navne-properties under bookings, som for loop med var c henter værdier fra
+        table = document.getElementById("myList1"),
+        data = bookingArray;
+
+    for (r = 0; r < data.length; r++) {
+        row = document.createElement('tr'); // tr = table rows. Looper over bookingArray og laver rækker for hvert element i arrayet
+
+        //laver 4 celler
+        for (c = 0; c < 4; c++) {
+            //  header = (document.createTextNode(tableHeader[c]));
+
+            cell = document.createElement('td'); //td =table data. Laver celler i tabel
+            text = document.createTextNode(data[r][prop[c]]); //laver række for hvert element: data[r], og inden i rækken laves celler med de værdier der tilhører propertiesene i bookingArray [prop[c]].
+            cell.appendChild(text); //indsætter data i cellen
+            row.appendChild(cell);  //indsætter celler i rækker
+            //table.appendChild(header);
+        }
+        //table.appendChild(createTable);
+        table.appendChild(row); //indsætter tabel i dokument
+    }
+}
+
+
+
+
+function myInformation() {
+    let i,
+        information =JSON.parse(localStorage.getItem("current_user")), //skiftes ud med identifier
+        infoArray = [];
+
+
+
+    let row, cell, text, r, c,
+        prop = ['bookingNumber','seatsChosen', 'date', 'time'], //navne-properties under bookings, som for loop med var c henter værdier fra
+        table = document.getElementById("myList1"),
+        data = bookingArray;
+
+    for (r = 0; r < data.length; r++) {
+        row = document.createElement('tr'); // tr = table rows. Looper over bookingArray og laver rækker for hvert element i arrayet
+
+        //laver 4 celler
+        for (c = 0; c < 4; c++) {
+            //  header = (document.createTextNode(tableHeader[c]));
+
+            cell = document.createElement('td'); //td =table data. Laver celler i tabel
+            text = document.createTextNode(data[r][prop[c]]); //laver række for hvert element: data[r], og inden i rækken laves celler med de værdier der tilhører propertiesene i bookingArray [prop[c]].
+            cell.appendChild(text); //indsætter data i cellen
+            row.appendChild(cell);  //indsætter celler i rækker
+            //table.appendChild(header);
+        }
+        //table.appendChild(createTable);
+        table.appendChild(row); //indsætter tabel i dokument
+    }
+
+
+}
---
+++ AktuelSystem/User/Users.js
@@ -0,0 +1,134 @@
+
+class user {
+    constructor (fullName, email, password, phone) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    createAlert() {
+        alert("Hi " + document.getElementById("fullName").text + " you have been registered");
+    }
+}
+
+
+function addUserInfo () {
+
+    var check, requirementsPassword, password, requirementsEmail, email, requirementsPhone, phone, info;
+
+    check = localStorage.length;
+    requirementsPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)^(?!@)[a-zA-Z\d]{8,}$/;
+    password = document.getElementById("password");
+    requirementsEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    email = document.getElementById("email");
+    requirementsPhone =/^\d{8}$/;
+    phone = document.getElementById("phoneNumber");
+
+    info = new user(document.getElementById("fullName").value, document.getElementById("email").value,
+        document.getElementById("password").value, document.getElementById("phoneNumber").value);
+
+    function storeLogin (values) {
+
+        values = JSON.parse(localStorage.getItem("info"));
+        if (values === null) {
+            values = [];
+        }
+        values.push(info);
+        localStorage.setItem("info", JSON.stringify(values));
+    }
+    /*
+    courseArray = JSON.parse(localStorage.getItem("info")) || [];
+    courseArray.push(info);
+    localStorage.setItem("info", JSON.stringify(courseArray));
+    courseArray = localStorage.getItem("info");
+    courseArray = JSON.parse(courseArray);
+    for (var i = 0; i < courseArray.length; i++) {
+        console.log(courseArray[i]);
+    }*/
+
+    if(document.getElementById("fullName").value !== "") {
+        document.getElementById("fullName").style.border = "none";
+        document.getElementById("fullNameValidation").innerHTML = "";
+    } else {
+        document.getElementById("fullName").style.border = "1px solid red";
+        document.getElementById("fullNameValidation").innerHTML="Please fill out";
+    }
+
+    if(password.value.match(requirementsPassword)) {
+        document.getElementById("password").style.border = "none";
+        document.getElementById("passwordValidation").innerHTML = "";
+    } else {
+        document.getElementById("password").style.border = "1px solid red";
+        document.getElementById("passwordValidation").innerHTML="Password must contain at least eight characters, " +
+            "one uppercase letter, one lowercase letter and one number";
+    }
+
+    if(email.value.match(requirementsEmail))
+    {
+        document.getElementById("email").style.border = "none";
+        document.getElementById("emailValidation").innerHTML="";
+
+
+    } else {
+        document.getElementById("email").style.border = "1px solid red";
+        document.getElementById("emailValidation").innerHTML="Please enter valid email";
+    }
+
+    if(phone.value.match(requirementsPhone))
+    {
+        document.getElementById("phoneValidation").innerHTML ="";
+        document.getElementById("phoneNumber").style.border = "none";
+    } else {
+        document.getElementById("phoneValidation").innerHTML ="Please enter valid phone number";
+        document.getElementById("phoneNumber").style.border = "1px solid red";
+
+    }
+
+    if(password.value.match(requirementsPassword) === null || email.value.match(requirementsEmail) === null || phone.value.match(requirementsPhone) === null)
+    {
+        return false;
+    }
+
+    if (info.email ==""|| info.password =="" || info.phone =="" || info.fullName =="")
+    {
+        return false;
+    }
+
+    if (info.email !== "" && info.password !== "" && check === 0) {
+
+        storeLogin();
+        alert("New user has been created");
+        window.location.replace("loginPage.html");
+
+    } else if (info.email !== "" && info.password !== "" &&
+        localStorage.getItem("info").indexOf(document.getElementById("email").value) == -1)
+    {
+        storeLogin();
+        alert("New user has been created");
+        new user().createAlert();
+        window.location.replace("loginPage.html");
+
+    } else {
+        alert("ERROR: User already exists");
+        return false;
+    }
+
+}
+
+
+class adminUser {
+    constructor (adminUserName, adminPassword) {
+        this.adminUserName = adminUserName;
+        this.adminPassword = adminPassword
+    }
+}
+
+
+var adminUser1 = new adminUser("admin1", "1234");
+
+function AdminUser () {
+    localStorage.setItem("adminUser", JSON.stringify(adminUser1));
+
+}
+
